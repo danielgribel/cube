4c91a1919e092d6fb1b767b47a4e4db48ac587af
Daniel Lemes Gribel <gribel.daniel@gmail.com>
2015-05-17 20:26:16 -0300

    reverse moves implementation

--- cube.py
+++ cube.py
@@ -105,36 +105,78 @@ def down():
     cube['back'][2,:] = rigth
     cube['left'][2,:] = back
     
-# reverse moves: we have to implement them to improve performance
+# reverse moves
 def front_():
-    front()
-    front()
-    front()
-    
+    update_face_('front')
+    #update other faces
+    up = np.copy(cube['up'][2,:])
+    rigth = np.copy(cube['rigth'][:,0])
+    down = np.copy(cube['down'][0,:])
+    left = np.copy(cube['left'][:,2])
+    cube['up'][2,:] = rigth
+    cube['rigth'][:,0] = down[::-1]
+    cube['down'][0,:] = left
+    cube['left'][:,2] = up[::-1]
+
 def up_():
-    up()
-    up()
-    up()
+    update_face_('up')
+    # update other faces
+    back = np.copy(cube['back'][0,:])
+    rigth = np.copy(cube['rigth'][0,:])
+    front = np.copy(cube['front'][0,:])
+    left = np.copy(cube['left'][0,:])
+    cube['back'][0,:] = rigth
+    cube['rigth'][0,:] = front
+    cube['front'][0,:] = left
+    cube['left'][0,:] = back
     
 def back_():
-    back()
-    back()
-    back()
+    update_face_('back')
+    # update other faces
+    up = np.copy(cube['up'][0,:])
+    left = np.copy(cube['left'][:,0])
+    down = np.copy(cube['down'][2,:])
+    rigth = np.copy(cube['rigth'][:,2])
+    cube['up'][0,:] = left[::-1]
+    cube['left'][:,0] = down
+    cube['down'][2,:] = rigth[::-1]
+    cube['rigth'][:,2] = up
     
 def left_():
-    left()
-    left()
-    left()
+    update_face_('left')
+    # update other faces
+    up = np.copy(cube['up'][:,0])
+    front = np.copy(cube['front'][:,0])
+    down = np.copy(cube['down'][:,0])
+    back = np.copy(cube['back'][:,2])
+    cube['up'][:,0] = front
+    cube['front'][:,0] = down
+    cube['down'][:,0] = back[::-1]
+    cube['back'][:,2] = up[::-1]
     
 def rigth_():
-    rigth()
-    rigth()
-    rigth()
+    update_face_('rigth')
+    # update other faces
+    up = np.copy(cube['up'][:,2])
+    back = np.copy(cube['back'][:,0])
+    down = np.copy(cube['down'][:,2])
+    front = np.copy(cube['front'][:,2])
+    cube['up'][:,2] = back[::-1]
+    cube['back'][:,0] = down[::-1]
+    cube['down'][:,2] = front
+    cube['front'][:,2] = up
 
 def down_():
-    down()
-    down()
-    down()
+    update_face_('down')
+    # update other faces
+    front = np.copy(cube['front'][2,:])
+    rigth = np.copy(cube['rigth'][2,:])
+    back = np.copy(cube['back'][2,:])
+    left = np.copy(cube['left'][2,:])
+    cube['front'][2,:] = rigth
+    cube['rigth'][2,:] = back
+    cube['back'][2,:] = left
+    cube['left'][2,:] = front
 
 def move(i):
     MOVE[i]()
@@ -183,15 +225,17 @@ node_id_source = hash(cPickle.dumps(cube))
 #     #print y, cont, len(moves), ("%s s" % (time.time() - start_time))
 
 cont = 0
+#nodes_on_graph = set()
 
-for i in range(0, 10):
+for i in range(0, 12):
     t = pow(2, i+1)
     for j in range(0, 100):
         initial_config()
         moves = [0] * t
         # execute uma DFS que parte da configuracao inicial e que termina apos visitar 2^i nos
-        visited2 = ["" for x in range(t+1)]
-        visited2[0] = node_id_source
+        visited2 = set()        
+        visited2.add(node_id_source)
+
         for k in range(1, t+1):
             cont = cont+1
             r = random.choice(range(-6,0) + range(1,7))
@@ -205,14 +249,16 @@ for i in range(0, 10):
                 move(r)
                 node_id = hash(cPickle.dumps(cube))
             moves[k-1] = r
-            visited2[k] = node_id
-    
+            visited2.add(node_id)
+
+            #nodes_on_graph.add(node_id)
+
         # execute uma BFS que parte de v e que termina ao alcancar a config inicial
-        for y in reversed(moves):
-            move(y*(-1))
+        #for y in reversed(moves):
+        #    move(y*(-1))
 
     print i+1, cont, t, ("%s s" % (time.time() - start_time))
-    
+  
 # for i in range(1, 100000):
 #     r = randint(1, 12)
 #     move(r)
